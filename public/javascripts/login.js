7d81831ec5cab1c235ea9a6bb65f53ca8a0cc0e0
ahmeditalia <34543951+ahmeditalia@users.noreply.github.com>
2019-01-01 03:13:08 +0200

    Revert "Delete login.js"
    This reverts commit a178292787956f29598bb022f61455bdb51dad22.

---
+++ public/javascripts/login.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    console.log("test 1");
+   $("#btn").click(()=>{
+       $.post("/getAllPositions",{id : 1});
+       // console.log("test 2");
+       // var user={
+       //     firstname: $('#first_name').val(),
+       //     lastname: $("#last_name").val(),
+       //     age: $("#age").val()
+       // };
+       // $.ajax({
+       //     url: "register",
+       //     type: "POST",
+       //     dataType: 'json',
+       //     contentType : 'application/json; charset=utf-8',
+       //     data: JSON.stringify(user),
+       //     success: (data) =>{
+       //         console.log("ajax success");
+       //     }
+       // });
+       // console.log("test 3");
+   });
+});
